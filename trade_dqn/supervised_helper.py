3a08bee69c9dde488aaac656396407d5d39b4e8f
shihuanzhao <shihuanzhao@tencent.com>
2018-08-20 16:10:35 +0800

    minor:

--- trade_dqn/supervised_helper.py
+++ trade_dqn/supervised_helper.py
@@ -3,13 +3,18 @@
 import numpy as np
 
 action_list = [1, 2]
-
+action_map = {1: 1,
+			 -1: 2}
 
 def generate_actions_from_price_data(price_list):
-    long_position = [1 if l < price_list[-1] else -1 for l in price_list[:-1]]
-    long_position = np.array(long_position)
-    final_profit = -np.sum(long_position * np.array(price_list)[:-1]) + np.sum(long_position) * price_list[-1]
-    return long_position, final_profit
+	long_position = [1 if l < price_list[-1] else -1 for l in price_list[:-1]]
+	long_position = np.array(long_position)
+	final_profit = -np.sum(long_position * np.array(price_list)[:-1]) + np.sum(long_position) * price_list[-1]
+	position = np.cumsum(long_position)
+	action_list = [action_map[l] for l in long_position]
+	golden_actions = zip(action_list, position)
+	golden_actions = [list(action) for action in golden_actions]
+	return golden_actions, final_profit
 
 
 def test():
